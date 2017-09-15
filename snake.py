53f42c444aa11d5ab0fae5cf69b3be98fe971fd7
Sirakorn Lamyai <tanersirakorn@gmail.com>
2017-09-15 10:28:25 +0700

    :tada: :art: Refactoring into objects, now snakes run.

--- snake.py
+++ snake.py
@@ -1,14 +1,29 @@
 import arcade
+from models import World
 
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
+
+class ModelSprite(arcade.Sprite):
+    def __init__(self, *args, **kwargs):
+        self.model = kwargs.pop('model', None)
+        super().__init__(*args, **kwargs)
+    def sync_with_model(self):
+        if self.model:
+            self.set_position(self.model.x, self.model.y)
+    def draw(self):
+        self.sync_with_model()
+        super().draw()
 
 class SnakeWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
         arcade.set_background_color((0,43,54))
-        
-        self.snake_sprite = arcade.Sprite('images/snake-block.png')
+        self.world = World(SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.snake_sprite = ModelSprite('images/snake-block.png', model=self.world.snake)
+        self.snake_sprite.set_position(300,300)
+    def update(self, delta):
+        self.world.update(delta)
     def on_draw(self):
         arcade.start_render()
         self.snake_sprite.draw()
